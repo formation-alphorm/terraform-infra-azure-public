8f68c8f0aaef80d0cc2890d974a441d209cac1a0
idianass94 <idi.anass.1994@gmail.com>
2024-12-02 00:43:10 +0100

    fix

--- tests/test_terraform_infra_test.go
+++ tests/test_terraform_infra_test.go
@@ -1,7 +1,7 @@
 package test
 
 import (
-	"encoding/json"
+	"strings"
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/terraform"
@@ -14,32 +14,42 @@ func TestTerraformInfrastructureVerification(t *testing.T) {
 		TerraformDir: "../terraform", // Chemin vers le répertoire Terraform
 	}
 
-	// Récupérer les outputs
-	rawOutput, err := terraform.OutputE(t, terraformOptions, "resource_group_name")
+	// Récupérer les outputs avec OutputRequiredE pour les chaînes brutes
+	outputResourceGroup, err := terraform.OutputRequiredE(t, terraformOptions, "resource_group_name")
 	if err != nil {
 		t.Fatalf("Failed to get output 'resource_group_name': %v", err)
 	}
 
-	// Décoder la sortie brute
-	var outputResourceGroup string
-	if err := json.Unmarshal([]byte(rawOutput), &outputResourceGroup); err != nil {
-		t.Fatalf("Failed to parse output 'resource_group_name': %v", err)
-	}
+	// Supprimer les guillemets autour des chaînes
+	outputResourceGroup = strings.Trim(outputResourceGroup, "\"")
 	t.Logf("Resource Group Name: %s", outputResourceGroup)
-
-	// Valider la valeur
 	assert.Equal(t, "terraform-iac-production-rg", outputResourceGroup)
 
-	// Ajoutez des assertions similaires pour les autres outputs
-	rawOutput, err = terraform.OutputE(t, terraformOptions, "acr_name")
+	outputAcrName, err := terraform.OutputRequiredE(t, terraformOptions, "acr_name")
 	if err != nil {
 		t.Fatalf("Failed to get output 'acr_name': %v", err)
 	}
 
-	var outputAcrName string
-	if err := json.Unmarshal([]byte(rawOutput), &outputAcrName); err != nil {
-		t.Fatalf("Failed to parse output 'acr_name': %v", err)
-	}
+	outputAcrName = strings.Trim(outputAcrName, "\"")
 	t.Logf("ACR Name: %s", outputAcrName)
 	assert.Equal(t, "iacterraformprojectproductionacr", outputAcrName)
+
+	// Ajoutez les autres outputs similaires
+	outputAppServicePlanName, err := terraform.OutputRequiredE(t, terraformOptions, "app_service_plan_name")
+	if err != nil {
+		t.Fatalf("Failed to get output 'app_service_plan_name': %v", err)
+	}
+
+	outputAppServicePlanName = strings.Trim(outputAppServicePlanName, "\"")
+	t.Logf("App Service Plan Name: %s", outputAppServicePlanName)
+	assert.Equal(t, "iac-production-app-service-plan", outputAppServicePlanName)
+
+	outputAppServiceName, err := terraform.OutputRequiredE(t, terraformOptions, "app_service_name")
+	if err != nil {
+		t.Fatalf("Failed to get output 'app_service_name': %v", err)
+	}
+
+	outputAppServiceName = strings.Trim(outputAppServiceName, "\"")
+	t.Logf("App Service Name: %s", outputAppServiceName)
+	assert.Equal(t, "iacprojectproductionapp", outputAppServiceName)
 }
